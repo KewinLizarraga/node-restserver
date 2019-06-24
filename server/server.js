c32c9ad4500a2d5ae6431a1d43d6cb3de607b57d
KewinLizarraga <kewin392@gmail.com>
2019-06-23 19:09:16 -0500

    add config and install package

--- server/server.js
+++ server/server.js
@@ -1,46 +1,23 @@
 require('./config/config');
 
 const express = require('express');
-const bodyParse = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
+
+const bodyParse = require('body-parser');
 
 // Parse application/x-www-form-urlencoded
 app.use(bodyParse.urlencoded({ extended: false}));
 // Parse application/json
 app.use(bodyParse.json());
 
+app.use(require('./routes/usuario'));
 
-
-app.get('/usuario', (req, res)=> {
-    res.json('get Usuario');
-});
-// POST --> para crear nuevos Registros
-app.post('/usuario', (req, res)=> {
-    let body = req.body;
-    
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-// PUT --> para actualizar los registros
-app.put('/usuario/:idx', (req, res)=> {
-    let id = req.params.idx;
-
-    res.json({
-        id
-    });
+// Connecting to MongoDB
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res)=> {
+    if (err) throw err;
+    console.log('Base de datos ONLINE en en puerto: 27017');
 });
 
-app.delete('/usuario', (req, res)=> {
-    res.json('delete Usuario');
-});
-
-app.listen(process.env.PORT, ()=> console.log(`Escuchando en el puerto ${process.env.PORT}`));
+app.listen(process.env.PORT, ()=> console.log(`Escuchando en el puerto: ${process.env.PORT}`));
